998baf22b752e6c5c4ba339bc76adafc85ba47ff
Nate Kinne <nathan.kinne@gmail.com>
2019-09-15 15:03:26 -0500

    still working on it

--- seeds/seed.seed_campsites.sql
+++ seeds/seed.seed_campsites.sql
@@ -2,7 +2,8 @@ BEGIN;
 
 TRUNCATE
   reviews,
-  campsites
+  campsites,
+  users
   RESTART IDENTITY CASCADE;
 
 -- insert new user values
@@ -27,7 +28,7 @@ INSERT INTO campsites(img, name, description, park, city, state)
     VALUES
         ('https://i.imgur.com/ELRFWHvb.jpg', 'Bayou place', 'A beautiful bayou great for fishing', 'Okefenokee Swamp National Park', 'Williamsburg', 'Georgia'),
         ('https://i.imgur.com/JGdoWLe.jpg', 'Mountain Place', 'Perched atop a mountain meadow this site offers great birdwatching', 'Black Mountain',  'Boise', 'Idaho'),
-        ('https://i.imgur.com/RhdiHIv.jpg', 'Mountain Place', 'In this beautiful valley a few miles down a dirt road offers a wonderful chance to find elk.', 'Mt. Yale','Salida', 'Colorado'),
+        ('https://i.imgur.com/RhdiHIv.jpg', 'Mountain Meadow', 'In this beautiful valley a few miles down a dirt road offers a wonderful chance to find elk.', 'Mt. Yale','Salida', 'Colorado'),
         ('https://i.imgur.com/H71AHhKb.jpg', 'Canyon Ledge', 'A gorgeous view of the grand canyon', 'Grand Canyon National Park','Phoenix', 'Arizona');
 
 -- populate reviews
