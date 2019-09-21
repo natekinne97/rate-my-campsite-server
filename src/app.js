e2917bcbe92bf1d7f49b71526ed1e784baeaad31
Nate Kinne <nathan.kinne@gmail.com>
2019-09-21 16:36:13 -0500

    dasf

--- src/app.js
+++ src/app.js
@@ -36,7 +36,9 @@ app.use('/api/users', usersRouter);
 app.use(function errorHandler(error, req, res, next) {
     let response
     if (NODE_ENV === 'production') {
-        response = { error: 'Server error' }
+        response = { error: 'Server error', 
+        message: "error something went wrong", 
+        errorMessage: error }
         // remove this when running
         console.log(error, 'something went wrong');
         res.json({
