e6d2c5bfab413d4a4feacd52f039345670bf22bb
Nate Kinne <nathan.kinne@gmail.com>
2019-09-21 16:21:42 -0500

    something

--- src/app.js
+++ src/app.js
@@ -36,8 +36,15 @@ app.use('/api/users', usersRouter);
 app.use(function errorHandler(error, req, res, next) {
     let response
     if (NODE_ENV === 'production') {
-        response = { error: 'Server error' }
+        response = { error: 'Server error', 
+        message: "error something went wrong", 
+        errorMessage: error }
+        // remove this when running
         console.log(error, 'something went wrong');
+        res.json({
+            error: error,
+            message: "something went wrong"
+                    })
     } else {
         console.error(error)
         response = { error: error.message, object: error }
